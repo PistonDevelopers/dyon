08e29b2a14fbcd6d527648e533c32d5a957fe871
Sven Nilsen <bvssvni@gmail.com>
2016-02-03 20:58:55 +0100

    Handle computed expressions when indexing objects

--- source/test.rs
+++ source/test.rs
@@ -1,6 +1,8 @@
 fn main() {
-    x := [0, 1, 2, 3, 4]
-    for i := 0; i < len(x); i += 1 {
+    x := {0: 0, 1: 1, 2: 2, 3: 3, 4: 4}
+    for i := 0; i < 5; i += 1 {
+        i := to_string(i)
+        println(x[i])
         x[i] += 1
     }
     println(x)
