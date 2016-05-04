0c819f02405ee389d851c937d6a115e830299df4
Sven Nilsen <bvssvni@gmail.com>
2016-05-04 19:34:38 +0200

    Fix return bug

--- source/test.rs
+++ source/test.rs
@@ -1,9 +1,11 @@
 
+fn foo() -> {
+    for i := 0; i < 10; i += 1 {
+        return 2
+    }
+    return 0
+}
+
 fn main() {
-    println("1")
-    sleep(1)
-    println("2")
-    sleep(2)
-    println("3")
-    sleep(3)
+    println(foo())
 }
