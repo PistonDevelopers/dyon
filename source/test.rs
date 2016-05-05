afbd543ac2ad070bc15c6325435ea03ce8c98559
Sven Nilsen <bvssvni@gmail.com>
2016-05-06 01:09:30 +0200

    Fixed some bugs in lifetime checker
    - Prevent containing self reference
    - Require reference to variable also when other lifetime than return,
    because or else there is no lifetime to compare

--- source/test.rs
+++ source/test.rs
@@ -1,5 +1,23 @@
+/*
+fn foo(mut a, b: 'a) {
+    a = b
+}
 
 fn main() {
-    m := unwrap(load("source/bench/n_body.rs"))
-    call(m, "main", [])
+    b := [[5]]
+    a := [[]]
+    foo(mut a, b)
+    println(a)
+    debug()
+
+    push(mut a, b)
+    println(a)
+}
+*/
+
+fn main() {
+    a := [5]
+    b := [6]
+    b = a
+    debug()
 }
