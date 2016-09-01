edbf531f90a5e766abfd9bf9945e79f8df518f76
Sven Nilsen <bvssvni@gmail.com>
2016-09-01 18:31:19 +0200

    Added more conversions for `Vec4`
    - Scale to 255 range for `[u8; 4]`

--- src/vec4.rs
+++ src/vec4.rs
@@ -99,6 +99,31 @@ impl From<(f64, f64, f64, f64)> for Vec4 {
     }
 }
 
+impl From<[u32; 2]> for Vec4 {
+    fn from(val: [u32; 2]) -> Vec4 {
+        Vec4([val[0] as f32, val[1] as f32, 0.0, 0.0])
+    }
+}
+
+impl From<(u32, u32)> for Vec4 {
+    fn from(val: (u32, u32)) -> Vec4 {
+        Vec4([val.0 as f32, val.1 as f32, 0.0, 0.0])
+    }
+}
+
+impl From<[u8; 4]> for Vec4 {
+    fn from(val: [u8; 4]) -> Vec4 {
+        Vec4([val[0] as f32 / 255.0, val[1] as f32 / 255.0,
+              val[2] as f32 / 255.0, val[3] as f32 / 255.0])
+    }
+}
+
+impl From<(u8, u8, u8, u8)> for Vec4 {
+    fn from(val: (u8, u8, u8, u8)) -> Vec4 {
+        Vec4([val.0 as f32 / 255.0, val.1 as f32 / 255.0,
+              val.2 as f32 / 255.0, val.3 as f32 / 255.0])
+    }
+}
 
 impl Into<[f32; 2]> for Vec4 {
     fn into(self) -> [f32; 2] {
@@ -169,5 +194,31 @@ impl Into<(f64, f64, f64)> for Vec4 {
 impl Into<(f64, f64, f64, f64)> for Vec4 {
     fn into(self) -> (f64, f64, f64, f64) {
         (self.0[0] as f64, self.0[1] as f64, self.0[2] as f64, self.0[3] as f64)
+    }
+}
+
+impl Into<[u32; 2]> for Vec4 {
+    fn into(self) -> [u32; 2] {
+        [self.0[0] as u32, self.0[1] as u32]
+    }
+}
+
+impl Into<(u32, u32)> for Vec4 {
+    fn into(self) -> (u32, u32) {
+        (self.0[0] as u32, self.0[1] as u32)
+    }
+}
+
+impl Into<(u8, u8, u8, u8)> for Vec4 {
+    fn into(self) -> (u8, u8, u8, u8) {
+        ((self.0[0] * 255.0) as u8, (self.0[1] * 255.0) as u8,
+         (self.0[2] * 255.0) as u8, (self.0[3] * 255.0) as u8)
+    }
+}
+
+impl Into<[u8; 4]> for Vec4 {
+    fn into(self) -> [u8; 4] {
+        [(self.0[0] * 255.0) as u8, (self.0[1] * 255.0) as u8,
+         (self.0[2] * 255.0) as u8, (self.0[3] * 255.0) as u8]
     }
 }
