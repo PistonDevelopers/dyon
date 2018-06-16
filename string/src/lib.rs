db18aaca1f0c947abcb60a8b118637e5b05b6308
Sven Nilsen <bvssvni@gmail.com>
2018-06-16 23:17:15 +0200

    Added "split"

--- string/src/lib.rs
+++ string/src/lib.rs
@@ -11,12 +11,32 @@ pub fn add_functions(module: &mut Module) {
         tys: vec![Type::Text],
         ret: Type::Array(Box::new(Type::Text)),
     });
+    module.add(Arc::new("split".into()), split, Dfn {
+        lts: vec![Lt::Default; 2],
+        tys: vec![Type::Text, Type::Array(Box::new(Type::Text))],
+        ret: Type::Array(Box::new(Type::Text)),
+    })
 }
 
 dyon_fn!{fn lines(text: Arc<String>) -> Variable {
     let mut arr = vec![];
     for line in text.lines() {
         arr.push(Variable::Text(Arc::new(line.into())));
+    }
+    Variable::Array(Arc::new(arr))
+}}
+
+dyon_fn!{fn split(text: Arc<String>, chs: Variable) -> Variable {
+    let mut arr = vec![];
+    if let Variable::Array(ref chs) = chs {
+        for line in text.split(|c| chs.iter().any(|v|
+            if let Variable::Text(ref txt) = *v {
+                if txt.chars().next() == Some(c) {true}
+                else {false}
+            } else {false}
+        )) {
+            arr.push(Variable::Text(Arc::new(line.into())));
+        }
     }
     Variable::Array(Arc::new(arr))
 }}
