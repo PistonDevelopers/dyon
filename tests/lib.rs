d0d516ff95aeb2683c151638fdf9fd1601fa6abe
Sven Nilsen <bvssvni@gmail.com>
2019-02-28 18:41:13 +0100

    Array of indices for lookup
    - Added “typechk/ind_arr.dyon”

--- tests/lib.rs
+++ tests/lib.rs
@@ -217,6 +217,7 @@ fn test_typechk() {
     test_fail_src("source/typechk/in_2.dyon");
     test_fail_src("source/typechk/vec4_2.dyon");
     test_fail_src("source/typechk/mat4_1.dyon");
+    test_src("source/typechk/ind_arr.dyon");
 }
 
 #[test]
