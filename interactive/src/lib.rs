64d45a5da57360be10c19ef2dec8939fd775f1d1
Sven Nilsen <bvssvni@gmail.com>
2021-05-27 22:41:36 +0200

    Added `stencil__outside`

--- interactive/src/lib.rs
+++ interactive/src/lib.rs
@@ -750,6 +750,10 @@ pub fn draw_2d<C: CharacterCache<Texture = G::Texture>, G: Graphics>(
                         let v: f64 = rt.var(&it[1])?;
                         c.draw_state.stencil = Some(Stencil::Inside(v as u8));
                     }
+                    "stencil__outside" => {
+                        let v: f64 = rt.var(&it[1])?;
+                        c.draw_state.stencil = Some(Stencil::Outside(v as u8));
+                    }
                     _ => {}
                 }
             }
